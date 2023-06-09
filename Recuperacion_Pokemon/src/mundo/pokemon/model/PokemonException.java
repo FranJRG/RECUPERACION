fd9a7ea967f9a0ac14833137bdf5aafcb5313ecc
FranJRG <115450041+FranJRG@users.noreply.github.com>
2023-06-09 14:14:17 +0200

    Add files via upload

---
+++ Recuperacion_Pokemon/src/mundo/pokemon/model/PokemonException.java
@@ -0,0 +1,18 @@
+package mundo.pokemon.model;
+
+public class PokemonException extends Exception {
+	
+
+	public PokemonException() {
+		
+	}
+	
+	public PokemonException(String string, Throwable throwable) {
+		
+	}
+	
+	public PokemonException(String string) {
+		
+	}
+	
+}
